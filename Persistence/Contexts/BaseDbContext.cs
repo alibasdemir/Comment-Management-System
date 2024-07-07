ea7d179e218141a9823e285b842370499a2611f6
alibasdemir <basdemir35@hotmail.com.tr>
2024-07-07 14:55:29 +0300

    add BaseDbContext to persistence layer and configure SQL Server connection

---
+++ Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Domain.Entities.Task> Tasks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-EGAQF63\\SQLEXPRESS;Initial Catalog=Database5.Comment-Management-Sys;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
