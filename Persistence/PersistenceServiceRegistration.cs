a8ea949562bf42ab5db2d8b13d73454acae6b596
alibasdemir <basdemir35@hotmail.com.tr>
2024-07-09 18:32:43 +0300

    refactor move DbContext registration to Persistence layer

---
+++ Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<BaseDbContext>();
+
+            return services;
+        }
+    }
+}
